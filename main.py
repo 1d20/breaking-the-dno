13382efb1f1445c5614889a073cb0835c7a6fa0f
Katadoka <katadoka@gmail.com>
2018-05-16 16:59:24 +0300

    /lyrics

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from models import Metronome
+from models import Metronome, Lyrics
 
 app = Flask(__name__,static_url_path="/static")
 
@@ -42,6 +42,24 @@ def hello():
 @app.route("/metronome") 
 def metronome():
     return render_template('metronome.html')
+
+@app.route("/lyrics") 
+def lyrics():
+    rows = list(Lyrics.select(Lyrics.title_text, Lyrics.id))
+    return render_template('lyrics.html', rows=rows)
+
+@app.route('/lyrics/delete/<ID>')
+def lyrics_delete_id(ID):
+    query = Lyrics.delete().where(Lyrics.id == ID)
+    query.execute()
+    return redirect(url_for('lyrics'))
+
+@app.route('/lyrics/create', methods=['POST'])
+def lyrics_create():
+    title_text = request.form['name']
+    text = request.form['content']
+    Lyrics.get_or_create(title_text=title_text, text=text)
+    return redirect(url_for('lyrics'))
 
 if (__name__ == "__main__"): 
     app.run(port = 5000) 
